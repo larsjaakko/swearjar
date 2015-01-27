cfd4bb932eb167c5650bc2983745fae613f70f84
Lars Jaakko Havro <larsjh@gmail.com>
2015-01-27 22:33:07 +0100

    Migrated to postgres

--- swearjar/settings.py
+++ swearjar/settings.py
@@ -60,8 +60,12 @@ WSGI_APPLICATION = 'swearjar.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'swearjar',
+        'USER': 'swearjar-user',
+        'PASSWORD': '',
+        'HOST': 'localhost',
+        'PORT': '5432',
     }
 }
 

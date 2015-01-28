0d73955ea7413ca8c84c55c9b79f739e518ef089
Lars Jaakko Havro <larsjh@gmail.com>
2015-01-28 21:48:53 +0100

    Added category and stem models

--- sweargen/models.py
+++ sweargen/models.py
@@ -1,3 +1,16 @@
 from django.db import models
 
 # Create your models here.
+
+
+class Category(models.Model):
+
+    name = models.CharField(max_length=128, unique=True)
+
+
+class Stem(models.Model):
+
+    category = models.ForeignKey(Category)
+    word = models.CharField(max_length=128, unique=True)
+    head = models.BooleanField(default=False)
+    modifier = models.BooleanField(default=False)

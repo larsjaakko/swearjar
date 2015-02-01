9c794f55f244568c9e464de0a76b3432c761ac61
Lars Jaakko Havro <larsjh@gmail.com>
2015-02-01 14:36:40 +0100

    Refactored to generic API views

--- sweargen/urls.py
+++ sweargen/urls.py
@@ -1,10 +1,13 @@
 from django.conf.urls import patterns, url
 from sweargen import views
+from rest_framework.urlpatterns import format_suffix_patterns
 
 
 urlpatterns = patterns(
     '',
     url(r'^$', views.index, name='index'),
-    url(r'^stems/$', views.stem_list),
-    url(r'^stems/(?P<pk>[0-9]+)/$', views.stem_detail),
+    url(r'^stems/$', views.StemList.as_view()),
+    url(r'^stems/(?P<pk>[0-9]+)/$', views.StemDetail.as_view()),
 )
+
+urlpatterns = format_suffix_patterns(urlpatterns)
--- sweargen/views.py
+++ sweargen/views.py
@@ -1,10 +1,10 @@
 from django.shortcuts import render
-from django.http import HttpResponse
-from django.views.decorators.csrf import csrf_exempt
-from rest_framework.renderers import JSONRenderer
-from rest_framework.parsers import JSONParser
+
 from sweargen.models import Stem
 from sweargen.serializers import StemSerializer
+
+from rest_framework import generics
+
 
 # Create your views here.
 
@@ -21,57 +21,11 @@ def index(request):
     return render(request, 'sweargen/index.html', context_dict)
 
 
-class JSONResponse(HttpResponse):
-    """
-    An HttpResponse that renders its content into JSON.
-    """
-    def __init__(self, data, **kwargs):
-        content = JSONRenderer().render(data)
-        kwargs['content_type'] = 'application/json'
-        super(JSONResponse, self).__init__(content, **kwargs)
+class StemList(generics.ListCreateAPIView):
+    queryset = Stem.objects.all()
+    serializer_class = StemSerializer
 
 
-@csrf_exempt
-def stem_list(request):
-    """
-    List all stems, or create a new stem.
-    """
-    if request.method == 'GET':
-        stems = Stem.objects.all()
-        serializer = StemSerializer(stems, many=True)
-        return JSONResponse(serializer.data)
-
-    elif request.method == 'POST':
-        data = JSONParser().parse(request)
-        serializer = StemSerializer(data=data)
-        if serializer.is_valid():
-            serializer.save()
-            return JSONResponse(serializer.data, status=201)
-        return JSONResponse(serializer.errors, status=400)
-
-
-@csrf_exempt
-def stem_detail(request, pk):
-    """
-    Retrieve, update or delete a code snippet.
-    """
-    try:
-        stem = Stem.objects.get(pk=pk)
-    except Stem.DoesNotExist:
-        return HttpResponse(status=404)
-
-    if request.method == 'GET':
-        serializer = StemSerializer(stem)
-        return JSONResponse(serializer.data)
-
-    elif request.method == 'PUT':
-        data = JSONParser().parse(request)
-        serializer = StemSerializer(snippet, data=data)
-        if serializer.is_valid():
-            serializer.save()
-            return JSONResponse(serializer.data)
-        return JSONResponse(serializer.errors, status=400)
-
-    elif request.method == 'DELETE':
-        stem.delete()
-        return HttpResponse(status=204)
+class StemDetail(generics.RetrieveUpdateDestroyAPIView):
+    queryset = Stem.objects.all()
+    serializer_class = StemSerializer

bc9cafe904fda72bb70325e7b895526b766d68c5
Lars Jaakko Havro <larsjh@gmail.com>
2016-08-01 21:38:57 +0200

    Added sitemap and google webmasters verification tag.

--- sweargen/urls.py
+++ sweargen/urls.py
@@ -1,6 +1,7 @@
 from django.conf.urls import patterns, url
 from sweargen import views
 from rest_framework.urlpatterns import format_suffix_patterns
+from django.contrib.sitemaps.views import sitemap
 
 
 urlpatterns = patterns(
@@ -11,6 +12,8 @@ urlpatterns = patterns(
     url(r'^sweargen/$', views.SwearGen.as_view()),
     url(r'^sweargen/(?P<category>.+)/$', views.SwearGen.as_view()),
     url(r'^stems/(?P<pk>[0-9]+)/$', views.StemDetail.as_view()),
+    url((r'^sitemap\.xml$', sitemap, {'sitemaps': sitemaps},
+        name='django.contrib.sitemaps.views.sitemap')),
 )
 
 urlpatterns = format_suffix_patterns(urlpatterns)
